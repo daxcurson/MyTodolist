14be779409cbefccea35d9816d2ff1f227d3c161
Agustín Villafañe <avillafa@droptek.com>
2020-09-06 18:57:34 -0300

    Added a screen to add tasks

---
+++ app/src/main/java/ar/com/strellis/mytodolist/AddTaskActivity.java
@@ -0,0 +1,48 @@
+package ar.com.strellis.mytodolist;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import ar.com.strellis.mytodolist.model.Task;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        Button button_save=(Button)findViewById(R.id.button_task_save);
+        TextView task_title=(TextView) findViewById(R.id.task_title);
+        TextView task_description=(TextView) findViewById(R.id.task_description);
+        EditText task_due_date=(EditText) findViewById(R.id.task_date_due);
+        button_save.setOnClickListener(v -> {
+            String title=task_title.getText().toString();
+            String description=task_description.getText().toString();
+            String due_date=task_due_date.getText().toString();
+            Task task=new Task();
+            task.setTitle(title);
+            task.setDescription(description);
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf=new SimpleDateFormat("d/m/Y");
+            try {
+                task.setDateDue(sdf.parse(due_date));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            MainActivity.database.taskDao().insert(task);
+            Snackbar.make(v, getString(R.string.task_saved), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            finish();
+        }
+        );
+    }
+}
--- app/src/main/java/ar/com/strellis/mytodolist/MainActivity.java
+++ app/src/main/java/ar/com/strellis/mytodolist/MainActivity.java
@@ -1,20 +1,20 @@
 package ar.com.strellis.mytodolist;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import ar.com.strellis.mytodolist.database.TaskDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static TaskDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +26,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // We'll launch the new task activity directly from here.
+                // Although we can add a menu here, we don't have a need for now.
+                Intent callAddTask=new Intent(MainActivity.this,AddTaskActivity.class);
+                callAddTask.setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+                startActivityForResult(callAddTask,200);
             }
         });
     }
